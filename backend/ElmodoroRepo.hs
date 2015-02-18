04acb7be11f2ce94effc6a935b17bd9f3c4ffe04
Ryan DV <rdevilla@uwaterloo.ca>
2015-02-18 16:48:11 -0500

    Implement createHandler and To/FromJSON instances
    Also makes ElmodoroDB acidic with SafeCopies.

--- backend/ElmodoroRepo.hs
+++ backend/ElmodoroRepo.hs
@@ -1,16 +1,21 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable, TemplateHaskell, TypeFamilies #-}
 module ElmodoroRepo where
 
 import Control.Monad.State.Lazy
 
 import Data.Acid
 import Data.IntMap.Lazy
+import Data.SafeCopy
 import Data.Time.Clock.POSIX
 import Data.Typeable
 
 import Elmodoro
 
 data ElmodoroDB = ElmodoroDB { allElmodoros :: IntMap Elmodoro } deriving(Typeable)
+
+$(deriveSafeCopy 0 'base ''ElmodoroStatus)
+$(deriveSafeCopy 0 'base ''Elmodoro)
+$(deriveSafeCopy 0 'base ''ElmodoroDB)
 
 createElmodoro          :: Elmodoro -> Update ElmodoroDB ()
 createElmodoro elmodoro = modify go where
@@ -22,3 +27,5 @@ createElmodoro elmodoro = modify go where
 updateElmodoro                     :: Key -> POSIXTime -> Elmodoro -> Update ElmodoroDB ()
 updateElmodoro id curtime elmodoro = modify go where
   go (ElmodoroDB db) = ElmodoroDB $ adjust (transitionElmodoro curtime) id db
+
+$(makeAcidic ''ElmodoroDB ['createElmodoro, 'updateElmodoro])
--- backend/Main.hs
+++ backend/Main.hs
@@ -1,2 +1,90 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.IO.Class
+
+import Data.Acid
+import Data.Aeson
+import qualified Data.ByteString.Char8 as C
+import Data.Char
+import Data.IntMap.Lazy
+import Data.Maybe
+import Data.Time.Clock
+import Data.Time.Clock.POSIX
+
+import Elmodoro
+import ElmodoroRepo
+
+import Happstack.Server
+
+import System.Environment
+
+instance ToJSON Elmodoro where
+  toJSON Elmodoro { startTime   = start
+                  , endTime     = end
+                  , workLength  = worklen
+                  , breakLength = breaklen
+                  , tags        = tags
+                  , status      = status
+                  } = object [ "starttime"   .= (floor start :: Int)
+                             , "endtime"     .= (floor <$> end :: Maybe Int)
+                             , "worklength"  .= ((floor worklen) :: Int)
+                             , "breaklength" .= ((floor breaklen) :: Int)
+                             , "tags"        .= tags
+                             , "status"      .= (Prelude.map toLower . show $ status)
+                             ]
+
+data ElmodoroRequest = ElmodoroRequest
+  { reqWorkLength  :: Int
+  , reqBreakLength :: Int
+  , reqTags        :: [String]
+  }
+
+instance FromJSON ElmodoroRequest where
+  parseJSON (Object o) =
+    ElmodoroRequest <$>
+      o .: "worklength"  <*>
+      o .: "breaklength" <*>
+      o .: "tags"
+
+createHandler :: ServerPart Response
+createHandler = do
+  req <- askRq
+  rqbody <- takeRequestBody req
+
+  if (isJust rqbody)
+    -- don't do this
+    then do
+      let reqelmodoro = decode (unBody . fromJust $ rqbody)
+      case reqelmodoro of
+        Just (elmodoro) -> do
+          curtime <- liftIO $ getPOSIXTime
+          let newelmodoro = Elmodoro { startTime = curtime
+            , endTime     = Nothing
+            , workLength  = fromInteger . toInteger . reqWorkLength $ elmodoro
+            , breakLength = fromInteger . toInteger . reqBreakLength $ elmodoro
+            , tags        = reqTags (elmodoro)
+            , status      = InProgress
+            }
+          ok $ toResponseBS (C.pack "application/json") (encode newelmodoro)
+        _ -> badRequest $ toResponse ("badRequest" :: String)
+    else internalServerError $ toResponse ("500" :: String)
+
+
+updateHandler :: Int -> ServerPart Response
+updateHandler = undefined
+
 main :: IO ()
-main = undefined
+main = do
+  db <- openLocalState (ElmodoroDB Data.IntMap.Lazy.empty)
+
+  envPort <- getEnv "PORT"
+  simpleHTTP (nullConf { port = read envPort }) $
+    msum [ dir "elmodoro" $
+      msum [ do nullDir
+                method POST
+                createHandler
+           , path updateHandler
+           ]
+          ]
