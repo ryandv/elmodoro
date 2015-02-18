5811127f8e6a9dffc17c1680186763ccebf4a431
Ryan DV <rdevilla@uwaterloo.ca>
2015-02-18 15:08:33 -0500

    Create ElmodoroRepo w/ create and update functions

---
+++ backend/ElmodoroRepo.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+module ElmodoroRepo where
+
+import Control.Monad.State.Lazy
+
+import Data.Acid
+import Data.IntMap.Lazy
+import Data.Time.Clock.POSIX
+import Data.Typeable
+
+import Elmodoro
+
+data ElmodoroDB = ElmodoroDB { allElmodoros :: IntMap Elmodoro } deriving(Typeable)
+
+createElmodoro          :: Elmodoro -> Update ElmodoroDB ()
+createElmodoro elmodoro = modify go where
+  go (ElmodoroDB db) = ElmodoroDB $
+    case maxViewWithKey db of
+      Just ((maxID, _), _) -> insert (maxID + 1) elmodoro db
+      Nothing              -> singleton 1 elmodoro
+
+updateElmodoro                     :: Key -> POSIXTime -> Elmodoro -> Update ElmodoroDB ()
+updateElmodoro id curtime elmodoro = modify go where
+  go (ElmodoroDB db) = ElmodoroDB $ adjust (endElmodoro curtime) id db
